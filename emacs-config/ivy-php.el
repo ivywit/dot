3ad9521fb0aa508f2b32dd27813283336fc288a2
Ivy Witter <c-pwitter@zulily.com>
2019-05-17 15:22:34 -0700

    updated files on Fri May 17 2019 15:22:34 GMT-0700 (PDT)

--- emacs-config/ivy-php.el
+++ emacs-config/ivy-php.el
@@ -3,11 +3,21 @@
 ;; this is a configuration file
 ;;
 ;;; Code:
+(use-package phpactor
+  :ensure t)
+
+(use-package company-phpactor
+  :ensure t)
 
 (use-package php-mode
   :mode "\\.php\\'"
   :interpreter "php"
-  :ensure t)
+  :ensure t
+  :defines company-backends
+  :hook ((php-mode . (lambda () (set (make-local-variable 'company-backends)
+                                     '(;; list of backends
+                                       company-phpactor
+                                       company-files))))))
 
 (use-package web-mode
   :mode ("\\.html?\\'" "\\.phtml\\'" "\\.tpl\\.php\\'" "\\.html\\.twig\\'" "\\.html\\.php\\'")
@@ -18,19 +28,6 @@
   (setq web-mode-markup-indent-offset 4)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
-
-(use-package phpactor
-  :ensure t
-  :hook (php-mode))
-
-(use-package company-phpactor
-  :ensure t
-  :hook (php-mode . (lambda ()
-                      (defvar company-backends)
-                      (set (make-local-variable 'company-backends)
-                           '(;; list of backends
-                             company-phpactor
-                             company-files)))))
 
 (provide 'ivy-php)
 ;;; ivy-php.el ends here
